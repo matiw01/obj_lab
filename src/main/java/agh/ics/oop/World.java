4cc72488232e1a92c699c971d9bb1bb4ed0c6815
Mateusz <mateusz.wiecek@interia.eu>
2021-10-26 20:28:30 +0200

    lab3

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -6,17 +6,14 @@ import java.util.Arrays;
 public class World {
     
     public static void main(String[] args) {
-//        Animal animal = new Animal();
-//        animal.move(MoveDirection.RIGHT);
-//        animal.move(MoveDirection.FORWARD);
-//        animal.move(MoveDirection.FORWARD);
-//        animal.move(MoveDirection.FORWARD);
-//        System.out.println(animal);
-        String[] directions = {"f","backward","right","false"};
+        Animal animal = new Animal();
+        String[] directions = {"f","backward","right","false","f"};
         OptionsParser parser = new OptionsParser();
         MoveDirection[] moveDirectons = parser.parse(directions);
-        System.out.println(Arrays.toString(moveDirectons));
-
+        for(MoveDirection direction : moveDirectons){
+            animal.move(direction);
+        }
+        System.out.println(animal);
     }
 
 
